4f9ac13b04ee61699892a9662224baa6547ad5ff
Prakash <shyaprakash@deloitte.com>
2021-05-13 09:39:40 -0700

    added snackbar messages on setup

--- src/app/pages/setup/setup.component.ts
+++ src/app/pages/setup/setup.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { debounceTime, switchMap } from 'rxjs/operators';
 import { SetupService } from '../../services/setup.service';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import { Constants } from 'src/app/shared/constant.component';
+
 @Component({
   selector: 'app-setup',
   templateUrl: './setup.component.html',
@@ -12,7 +15,8 @@ export class SetupComponent implements OnInit {
   ftpServerData: any;
   cloudServerConfig: any;
   cloudServerData: any;
-  constructor(private setupService: SetupService) { }
+  constructor(private setupService: SetupService,
+    private _snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
     this.ftpServerConfig = this.setupService.getFtpServerConfig();
@@ -31,39 +35,55 @@ export class SetupComponent implements OnInit {
   submitFtpDetails(event) {
    this.setupService.postFtpServerData(event).subscribe(res => {
      this.setupService.requeryFTPServerDetails();
+     this._snackBar.open(Constants.ftpSetupSave,null, {
+      duration: 2000
+    });
    });
   }
 
   updateFtpDetails(event) {
     this.setupService.putFtpServerData(event).subscribe(res => {
       this.setupService.requeryFTPServerDetails();
+      this._snackBar.open(Constants.ftpSetupUpdate,null, {
+        duration: 2000
+      });
     });
-
   }
 
   deleteFtpDetails(event) {
     console.log(event);
     this.setupService.deleteFtpServerData(event).subscribe(res => {
       this.setupService.requeryFTPServerDetails();
+      this._snackBar.open(Constants.ftpSetupDelete,null, {
+        duration: 2000
+      });
     });
-
   }
 
   submitCloudDetails(event) {
-     this.setupService.postCloudServerData(event).subscribe(res => {
-      this.setupService.requeryCloudServerDetails();
+    this.setupService.postCloudServerData(event).subscribe(res => {
+    this.setupService.requeryCloudServerDetails();
+    this._snackBar.open(Constants.cloudInstanceSave,null, {
+      duration: 2000
+    });
     });
   }
 
   updateCloudDetails(event) {
     this.setupService.putCloudServerData(event).subscribe(res => {
       this.setupService.requeryCloudServerDetails();
+      this._snackBar.open(Constants.cloudInstanceUpdate,null, {
+        duration: 2000
+      });
     });
   }
 
   deleteCloudDetails(event) {
     this.setupService.deleteCloudServerData(event).subscribe(res => {
       this.setupService.requeryCloudServerDetails();
+      this._snackBar.open(Constants.cloudInstanceDelete,null, {
+        duration: 2000
+      });
     });
   }
 }
---
+++ src/app/shared/constant.component.ts
@@ -0,0 +1,15 @@
+export class Constants {
+    public static ftpSetupSave='FTP Server setup successfully saved';
+    public static ftpSetupUpdate='FTP Server setup successfully updated';
+    public static ftpSetupDelete='FTP Server setup successfully deleted';
+
+    public static cloudInstanceSave='Cloud Instance Setup successfully saved';
+    public static cloudInstanceUpdate='Cloud Instance Setup successfully updated';
+    public static cloudInstanceDelete='Cloud Instance Setup successfully deleted';
+
+    public static udaSave='UDA Mappings successfully saved';
+    public static udaUpdate='UDA Mappings successfully updated';
+    public static udaDelete='UDA Mappings successfully deleted';
+    public static udaUpload='UDA Mappings successfully uploaded';
+
+}
