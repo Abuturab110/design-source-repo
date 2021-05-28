8a3791492f9c4bd9c07e810fa0c88b3c8f0efa25
Prakash <shyaprakash@deloitte.com>
2021-05-28 06:09:51 -0700

    worked on graph for dashboard

--- routes/purchaseCatalog.js
+++ routes/purchaseCatalog.js
@@ -2,39 +2,40 @@ const utils = require('../utils/general-utilities')
 var express = require('express');
 var router = express.Router();
 const dbSet = require('../utils/db')
-, itemClassConvDB = dbSet.itemClassConvDB;
+, purchasingCatalogDB = dbSet.purchasingCatalogDB;
 const base64 = require('file-base64');
 const request = require('request');
 const Client = require('ssh2-sftp-client');
 
-router.get('/getItemClassConvDetails', function(req, res, next) {
-  itemClassConvDB.find({}, function (err, docs) {
+router.get('/getPurchaseCatalogDetails', function(req, res, next) {
+  purchasingCatalogDB.find({}, function (err, docs) {
         if (err) return next(err);
         res.json(docs);
     });
 });
 
-router.post('/postItemClassConvDetails', function(req, res, next) {
+router.post('/postPurchaseCatalogDetails', function(req, res, next) {
   let postData = {...req.body, "creation-date": new Date().toString(), "last-update-date": new Date().toString()};
-  itemClassConvDB.insert(postData, function (err, docs) {
+  purchasingCatalogDB.insert(postData, function (err, docs) {
     if (err) return next(err);
      console.log('files inserted successfully' + JSON.stringify(req.body));
      res.json(docs);
   });
 });
 
-router.put('/putItemClassConvDetails', function (req, res, next) {
-  itemClassConvDB.update({_id:req.body._id},{ $set: req.body },{}, function (err, docs) {
+router.put('/putPurchaseCatalog', function (req, res, next) {
+  purchasingCatalogDB.update({_id:req.body._id},{ $set: req.body },{}, function (err, docs) {
      if (err) return next(err);
      res.json('{"MSG":"SUCCESS"}');
   });
 });
 
-router.delete('/deleteItemConvClassDetails/:id', function (req, res, next) {
-  itemClassConvDB.remove({_id:req.params.id},{}, function (err, docs) {
+router.delete('/deletePurchaseCatalog/:id', function (req, res, next) {
+  purchasingCatalogDB.remove({_id:req.params.id},{}, function (err, docs) {
      if (err) return next(err);
      res.json('{"MSG":"SUCCESS"}');
   });
 });
+
 
 module.exports = router;
