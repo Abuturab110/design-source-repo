51e565497ac7d02a00c6b2fc5acc112e0cfd7be7
Prakash <shyaprakash@deloitte.com>
2021-06-04 07:13:15 -0700

    removed commenetd code

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -268,19 +268,8 @@ const sortByDate = arr => {
     let date = newDate.getDate() < 10 ? '0'+newDate.getDate() : newDate.getDate();
     let hours = newDate.getHours() < 10 ? '0'+newDate.getHours() : newDate.getHours()
     let minutes = newDate.getMinutes() < 10 ? '0'+newDate.getMinutes(): newDate.getMinutes()
-    //return `${month}/${date} ${hours}:${minutes}`
     return `${month}/${date}`
    
 }
 
-// function formatLineGraphDate(dateString) {
-//   let newDate = new Date(dateString)
-//   let month = (newDate.getMonth() + 1) < 10 ? '0'+(newDate.getMonth() + 1) : (newDate.getMonth() + 1)
-//   let date = newDate.getDate() < 10 ? '0'+newDate.getDate() : newDate.getDate();
-//   let hours = newDate.getHours() < 10 ? '0'+newDate.getHours() : newDate.getHours()
-//   let minutes = newDate.getMinutes() < 10 ? '0'+newDate.getMinutes(): newDate.getMinutes()
-//     return `${month}/${date}`
-// }
-
-
-  module.exports = router;
+module.exports = router;
